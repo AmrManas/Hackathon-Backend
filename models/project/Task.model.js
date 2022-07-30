00d0ac545345178363fc64f932453ad5762a6511
manas_codes <manas.mishra@zoof.co.in>
2022-07-31 04:38:47 +0530

    add time in task

--- models/project/Task.model.js
+++ models/project/Task.model.js
@@ -5,6 +5,10 @@ const taskSchema = new Schema(
     title: { type: String, required: true },
     description: { type: String },
     panel: { default: "todo", type: String, enum: ["todo", "doing", "done"] },
+    timeSheet: {
+      startTime: { type: Date },
+      endTime: { type: Date },
+    },
   },
   {
     timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
