000a54317328976df3b89e06200e7401f29bb2b1
manas_codes <manas.mishra@zoof.co.in>
2022-07-30 10:50:38 +0530

    fix user schema

--- models/User.js
+++ models/User.js
@@ -2,14 +2,8 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema(
   {
     email: { type: String, unique: true, required: true },
-  },
-  {
     username: { type: String, unique: true, required: true },
-  },
-  {
     password: { type: String, unique: true, required: true },
-  },
-  {
     phoneNumber: { type: String, unique: true, required: true },
   },
   {
