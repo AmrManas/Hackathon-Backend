6df074fecf9b18ffba7a2e65706801600560479c
gurpanth-braham-12 <gurpanthjeet.singh@simbaquartz.com>
2022-07-31 13:58:43 +0530

    changes

--- controllers/projects/tasks/updateTask.js
+++ controllers/projects/tasks/updateTask.js
@@ -6,37 +6,23 @@ const updateTask = async (req, res, next) => {
   const { body } = req;
 
   try {
-    const checkTask = await Task.find({
-      panel: "doing",
-    });
+    const updateTask = await Task.findByIdAndUpdate(
+      {
+        _id: id,
+      },
+      body
+    );
 
-    console.log(checkTask);
-
-    if (body?.panel === "doing") {
-      if (checkTask) {
-        throw createError.Conflict(
-          `You already have an item in doing section. Please Complete it first.`
-        );
-      }
-    } else {
-      const updateTask = await Task.findByIdAndUpdate(
-        {
-          _id: id,
-        },
-        body
+    if (!updateTask) {
+      throw createError.InternalServerError(
+        "Your request could not be processed. Please contact support or try again after some time."
       );
-
-      if (!updateTask) {
-        throw createError.InternalServerError(
-          "Your request could not be processed. Please contact support or try again after some time."
-        );
-      }
-
-      res.status(200).send({
-        message: "success",
-        data: updateTask,
-      });
     }
+
+    res.status(200).send({
+      message: "success",
+      data: updateTask,
+    });
   } catch (err) {
     console.log(err);
     next(err);
