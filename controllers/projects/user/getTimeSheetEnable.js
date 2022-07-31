25fc0341890b7e7533c997982e98186598701ffe
manas_codes <manas.mishra@zoof.co.in>
2022-07-31 12:10:04 +0530

    test changes

--- controllers/projects/user/getTimeSheetEnable.js
+++ controllers/projects/user/getTimeSheetEnable.js
@@ -1,14 +1,16 @@
 const User = require("../../../models/user/User.model");
+const jwt = require("jsonwebtoken");
 const createError = require("http-errors");
 
 const getTimeSheetEnable = async (req, res, next) => {
   const { id } = req.headers;
 
   try {
-    const decryptedId = atob(id);
+    const decoded = jwt.decode(id, { complete: true });
+    console.log("decoded.payload", decoded.payload);
 
     const getTimeSheetEnable = await User.findOne({
-      _id: decryptedId,
+      _id: decoded.payload._id,
     });
 
     if (!getTimeSheetEnable) {
